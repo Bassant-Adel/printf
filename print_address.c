c6e84b1f9204213ec9c59ded0de02d5c0d2dd1c8
Nayra-Adel19 <nayra.adel19@gmail.com>
2023-08-23 01:11:15 +0300

    Add print_address

---
+++ print_address.c
@@ -0,0 +1,21 @@
+
+/**
+ * print_address - Prints hexadecimal representation of an address
+ *@list: The argument pointer
+ *@params: The params_t struct
+ *Return: The bytes printed
+ */
+
+int print_address(va_list list, params_t *params)
+{
+	char *str;
+	unsigned long int num = va_arg(list, unsigned long int);
+
+	if (!num)
+		return (_puts("(nil)"));
+
+	str = convert(num, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
