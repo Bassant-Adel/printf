3056a48310c7560130e4abbaa62a1a086a8d4ea6
Nayra-Adel19 <nayra.adel19@gmail.com>
2023-08-23 01:14:30 +0300

    Add print_unsigned

--- print_address.c
+++ print_address.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * print_address - Prints hexadecimal representation of an address
---
+++ print_unsigned.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_unsigned - Prints unsigned number
+ *@list: The va_list pointer
+ *@params: The params_t struct
+ *Return: A bytes printed
+ */
+
+int print_unsigned(va_list list, params_t *params)
+{
+	unsigned long num;
+
+	if (params->l_modifier)
+		num = (unsigned long)va_arg(list, unsigned long);
+	else if (params->h_modifier)
+		num = (unsigned short int)va_arg(list, unsigned int);
+	else
+		num = (unsigned int)va_arg(list, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(num, 10, CONVERT_UNSIGNED, params), params));
+}
