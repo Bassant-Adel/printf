750bee7547e89888991904bf5113946215ddc2fb
Nayra-Adel19 <nayra.adel19@gmail.com>
2023-08-23 01:08:01 +0300

    Add print_percent

---
+++ print_percent.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_percent - prints a percent sign ("%")
+ *@list: A pointer
+ *@params: A struct
+ *Return: A number chars printed
+ */
+
+int print_percent(va_list list, params_t *params)
+{
+	(void)list;
+	(void)params;
+	return (_putchar('%'));
+}
