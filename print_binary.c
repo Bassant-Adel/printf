d935f1838987a4566df421345b2965eea1afac4b
Nayra-Adel19 <nayra.adel19@gmail.com>
2023-08-23 00:59:41 +0300

    Add print_binary

---
+++ print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints binary representation
+ *@list: The argument pointer
+ *@params: The parameters struct
+ *Return: The bytes printed
+ */
+
+int print_binary(va_list list, params_t *params)
+{
+	char *str = convert(num, 2, CONVERT_UNSIGNED, params);
+	int ch = 0;
+	unsigned int num = va_arg(list, unsigned int);
+
+	if (params->hashtag_flag && num)
+		*--str = '0';
+	params->unsign = 1;
+	return (ch += print_number(str, params));
+}
