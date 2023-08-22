844409faf18590ce68f55f2725f48759c7019e45
Bassant-Adel <baadel404@gmail.com>
2023-08-22 13:17:31 -0700

    Create New File -> Precision :D

---
+++ precision.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * get_precision - The function is used to extract
+ *the precision value from the format string
+ *
+ *@p: It's a String
+ *@params: It's a Parameters
+ *@list: It's an Argument
+ *Return: The New Pointer
+ */
+
+char *get_precision(char *p, params_t *params, va_list list)
+{
+
+	int n = 0;
+
+	if (*p != '.')
+	{
+
+		return (p);
+
+	}	p++;
+
+	if (*p == '*')
+	{
+		n = va_arg(list, int);
+		p++;
+	}
+
+	else
+	{
+
+		while (_isdigit(*p))
+		{
+			n = n * 10 + (*p++ - '0');
+		}
+
+	}	params->precision = n;
+
+	return (p);
+
+}
